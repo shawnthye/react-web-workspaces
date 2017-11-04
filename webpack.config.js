86590bf44f34c4b9153a5d59a5375a89b48552f0
Shawn Thye <shawn.thye@gmail.com>
2017-11-05 04:25:33 +0800

    webpack directly dist into packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,15 @@
 'use strict';
 
-//process.env.BABEL_ENV = 'development';
-
-const path = require('path');
-
-const OUT_PATH = path.resolve('./build');
-const PUBLIC_PATH = '/assets/';
-const DEVTOOL = 'source-map';
-
 const IS_DEV = process.env.BABEL_ENV === 'development';
 const IS_PROD = false;
 
-module.exports = [];
-// module.exports = [{
-//     name: 'js-all',
-//     entry: path.resolve('./packages/react-material-components/index.js'),
-//     output: {
-//         path: OUT_PATH,
-//         publicPath: PUBLIC_PATH,
-//         filename: 'react-material-components.' + (IS_DEV ? '' : 'min.') + 'js',
-//         libraryTarget: 'umd',
-//         library: 'mdc',
-//     },
-//     // See https://github.com/webpack/webpack-dev-server/issues/882
-//     // Because we only spin up dev servers temporarily, and all of our assets are publicly
-//     // available on GitHub, we can safely disable this check.
-//     devServer: {
-//         disableHostCheck: true,
-//     },
-//     devtool: DEVTOOL,
-//     module: {
-//         rules: [{
-//             test: /\.js$/,
-//             exclude: [/(node_modules)/],
-//             loader: 'babel-loader',
-//             options: {
-//                 cacheDirectory: true,
-//                 presets: [
-//                     'react-app'
-//                 ]
-//             },
-//         }],
-//     }
-// }];
+const path = require('path');
 
-module.exports.push({
+const OUT_PATH = path.resolve(__dirname, 'packages');
+const PUBLIC_PATH = '/assets/';
+const DEVTOOL = IS_DEV ? 'source-map' : false
+
+module.exports = [{
     name: 'js-components',
     entry: {
         button: [path.resolve('./packages/button/index.js')],
@@ -53,9 +18,9 @@ module.exports.push({
     output: {
         path: OUT_PATH,
         publicPath: PUBLIC_PATH,
-        filename: '[name].' + (IS_DEV ? '' : 'min.') + 'js',
+        filename: '[name]/dist/index.' + (IS_DEV ? '' : 'min.') + 'js',
         libraryTarget: 'umd',
-        library: ['mdc', '[name]'],
+        library: ['react', '[name]']
     },
     devtool: DEVTOOL,
     module: {
@@ -71,6 +36,37 @@ module.exports.push({
             }
         }],
     },
-    plugins: [
-    ],
+    plugins: [],
+}];
+
+module.exports.push({
+    name: 'js-all',
+    entry: path.resolve('./packages/react-material-components/index.js'),
+    output: {
+        path: OUT_PATH,
+        publicPath: PUBLIC_PATH,
+        filename: 'react-material-components/dist/index.' + (IS_DEV ? '' : 'min.') + 'js',
+        libraryTarget: 'umd',
+        library: ['react', '[name]']
+    },
+    // See https://github.com/webpack/webpack-dev-server/issues/882
+    // Because we only spin up dev servers temporarily, and all of our assets are publicly
+    // available on GitHub, we can safely disable this check.
+    devServer: {
+        disableHostCheck: true,
+    },
+    devtool: DEVTOOL,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: [/(node_modules)/],
+            loader: 'babel-loader',
+            options: {
+                cacheDirectory: true,
+                presets: [
+                    'react-app'
+                ]
+            },
+        }],
+    }
 });
